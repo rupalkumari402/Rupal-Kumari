2c401316b0a6b85871aab01d0f2d1f174f204761
Rupal Kumari <rupalkumari0987@gmail.com>
2021-03-21 13:22:59 +0530

    initial-commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularProject'`, () => {
+  it(`should have as title 'rupal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularProject');
+    expect(app.title).toEqual('rupal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularProject app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('rupal app is running!');
   });
 });
